1eaf33781e53a134abc3789b1b5bbd47eb7053e5
josevbrito <josevictorbritoc@gmail.com>
2025-06-18 01:02:37 -0300

    feat: :zap: Add Seeders

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'is_active' => true,
+            'is_2fa_enabled' => false,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Regular User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+            'is_active' => true,
+        ]);
+
+        User::factory(10)->create();
+    }
+}
